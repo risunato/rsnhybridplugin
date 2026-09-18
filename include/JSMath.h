371923da85eabdc8201109049c19e899b8463016
Risunato <risunato@example.com>
2026-09-18 22:57:04 +0700

    WIP: Migrasi Logika C++

--- include/JSMath.h
+++ include/JSMath.h
@@ -5,9 +5,9 @@
 namespace JSMath {
     // Meniru Math.random() dari JavaScript (mereturn float 0.0 hingga < 1.0)
     inline float random() {
-        static std::random_device rd;
-        static std::mt19937 gen(rd());
-        static std::uniform_real_distribution<float> dis(0.0f, 1.0f);
+        thread_local std::random_device rd;
+        thread_local std::mt19937 gen(rd());
+        thread_local std::uniform_real_distribution<float> dis(0.0f, 1.0f);
         return dis(gen);
     }
 }
