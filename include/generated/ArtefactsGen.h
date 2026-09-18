f5395e5093a4fff8f533821c93ebd96c7452be3f
Risunato <risunato@example.com>
2026-09-18 22:50:58 +0700

    WIP: Migrasi Logika C++

--- include/generated/ArtefactsGen.h
+++ include/generated/ArtefactsGen.h
@@ -1,8 +1,47 @@
 #pragma once
 #include <endstone/player.h>
-#include <endstone/entity/entity.h>
-#include <cmath>
+#include <endstone/server.h>
 
 namespace ArtefactsGen {
     void init();
+    void handle_artefactCooldown(endstone::Player& p);
+    void handle_blastFungus(endstone::Player& p);
+    void handle_buzzyNest(endstone::Player& p);
+    void handle_corruptedBeacon(endstone::Player& p);
+    void handle_corruptedPumpkin(endstone::Player& p);
+    void handle_corruptedSeeds(endstone::Player& p);
+    void handle_deathCap(endstone::Player& p);
+    void handle_enchantersTome(endstone::Player& p);
+    void handle_enchantedGrass(endstone::Player& p);
+    void handle_eyeGuardian(endstone::Player& p);
+    void handle_ghostCloak(endstone::Player& p);
+    void handle_golemKit(endstone::Player& p);
+    void handle_gongWeakening(endstone::Player& p);
+    void handle_harvester(endstone::Player& p);
+    void handle_iceWand(endstone::Player& p);
+    void handle_ironHide(endstone::Player& p);
+    void handle_lightFeather(endstone::Player& p);
+    void handle_lightningRod(endstone::Player& p);
+    void handle_powershaker(endstone::Player& p);
+    void handle_satchelElements(endstone::Player& p);
+    void handle_satchelElixirs(endstone::Player& p);
+    void handle_satchelSnacks(endstone::Player& p);
+    void handle_scatterMines(endstone::Player& p);
+    void handle_shadowShifter(endstone::Player& p);
+    void handle_shockPowder(endstone::Player& p);
+    void handle_soulHealer(endstone::Player& p);
+    void handle_soulLantern(endstone::Player& p);
+    void handle_spinblade(endstone::Player& p);
+    void handle_swiftnessBoot(endstone::Player& p);
+    void handle_totemCasting(endstone::Player& p);
+    void handle_totemShielding(endstone::Player& p);
+    void handle_totemRegeneration(endstone::Player& p);
+    void handle_updraftTome(endstone::Player& p);
+    void handle_vexingChant(endstone::Player& p);
+    void handle_windHorn(endstone::Player& p);
+    void handle_tomeDuplication(endstone::Player& p);
+    void handle_loveMedallion(endstone::Player& p);
+    void handle_totemSoulProtection(endstone::Player& p);
+    void handle_tastyBone(endstone::Player& p);
+    void handle_wonderfulWheat(endstone::Player& p);
 }
--- src/ArtefactsHelper.cpp
+++ src/ArtefactsHelper.cpp
@@ -1,38 +1,63 @@
 #include "ArtefactsHelper.h"
+#include "generated/ArtefactsGen.h"
+#include <endstone/server.h>
+#include <unordered_map>
+#include <functional>
 
 namespace ArtefactsHelper {
+    using ArtefactHandlerMap = std::unordered_map<std::string, std::function<void(endstone::Player&)>>;
+    static ArtefactHandlerMap artefactHandlers;
+
+    void initArtefactMap() {
+        if (!artefactHandlers.empty()) return;
+        
+        artefactHandlers["dungeons:artefactCooldown"] = [](endstone::Player& p) { ArtefactsGen::handle_artefactCooldown(p); };
+        artefactHandlers["dungeons:blastFungus"] = [](endstone::Player& p) { ArtefactsGen::handle_blastFungus(p); };
+        artefactHandlers["dungeons:buzzyNest"] = [](endstone::Player& p) { ArtefactsGen::handle_buzzyNest(p); };
+        artefactHandlers["dungeons:corruptedBeacon"] = [](endstone::Player& p) { ArtefactsGen::handle_corruptedBeacon(p); };
+        artefactHandlers["dungeons:corruptedPumpkin"] = [](endstone::Player& p) { ArtefactsGen::handle_corruptedPumpkin(p); };
+        artefactHandlers["dungeons:corruptedSeeds"] = [](endstone::Player& p) { ArtefactsGen::handle_corruptedSeeds(p); };
+        artefactHandlers["dungeons:deathCap"] = [](endstone::Player& p) { ArtefactsGen::handle_deathCap(p); };
+        artefactHandlers["dungeons:enchantersTome"] = [](endstone::Player& p) { ArtefactsGen::handle_enchantersTome(p); };
+        artefactHandlers["dungeons:enchantedGrass"] = [](endstone::Player& p) { ArtefactsGen::handle_enchantedGrass(p); };
+        artefactHandlers["dungeons:eyeGuardian"] = [](endstone::Player& p) { ArtefactsGen::handle_eyeGuardian(p); };
+        artefactHandlers["dungeons:ghostCloak"] = [](endstone::Player& p) { ArtefactsGen::handle_ghostCloak(p); };
+        artefactHandlers["dungeons:golemKit"] = [](endstone::Player& p) { ArtefactsGen::handle_golemKit(p); };
+        artefactHandlers["dungeons:gongWeakening"] = [](endstone::Player& p) { ArtefactsGen::handle_gongWeakening(p); };
+        artefactHandlers["dungeons:harvester"] = [](endstone::Player& p) { ArtefactsGen::handle_harvester(p); };
+        artefactHandlers["dungeons:iceWand"] = [](endstone::Player& p) { ArtefactsGen::handle_iceWand(p); };
+        artefactHandlers["dungeons:ironHide"] = [](endstone::Player& p) { ArtefactsGen::handle_ironHide(p); };
+        artefactHandlers["dungeons:lightFeather"] = [](endstone::Player& p) { ArtefactsGen::handle_lightFeather(p); };
+        artefactHandlers["dungeons:lightningRod"] = [](endstone::Player& p) { ArtefactsGen::handle_lightningRod(p); };
+        artefactHandlers["dungeons:powershaker"] = [](endstone::Player& p) { ArtefactsGen::handle_powershaker(p); };
+        artefactHandlers["dungeons:satchelElements"] = [](endstone::Player& p) { ArtefactsGen::handle_satchelElements(p); };
+        artefactHandlers["dungeons:satchelElixirs"] = [](endstone::Player& p) { ArtefactsGen::handle_satchelElixirs(p); };
+        artefactHandlers["dungeons:satchelSnacks"] = [](endstone::Player& p) { ArtefactsGen::handle_satchelSnacks(p); };
+        artefactHandlers["dungeons:scatterMines"] = [](endstone::Player& p) { ArtefactsGen::handle_scatterMines(p); };
+        artefactHandlers["dungeons:shadowShifter"] = [](endstone::Player& p) { ArtefactsGen::handle_shadowShifter(p); };
+        artefactHandlers["dungeons:shockPowder"] = [](endstone::Player& p) { ArtefactsGen::handle_shockPowder(p); };
+        artefactHandlers["dungeons:soulHealer"] = [](endstone::Player& p) { ArtefactsGen::handle_soulHealer(p); };
+        artefactHandlers["dungeons:soulLantern"] = [](endstone::Player& p) { ArtefactsGen::handle_soulLantern(p); };
+        artefactHandlers["dungeons:spinblade"] = [](endstone::Player& p) { ArtefactsGen::handle_spinblade(p); };
+        artefactHandlers["dungeons:swiftnessBoot"] = [](endstone::Player& p) { ArtefactsGen::handle_swiftnessBoot(p); };
+        artefactHandlers["dungeons:totemCasting"] = [](endstone::Player& p) { ArtefactsGen::handle_totemCasting(p); };
+        artefactHandlers["dungeons:totemShielding"] = [](endstone::Player& p) { ArtefactsGen::handle_totemShielding(p); };
+        artefactHandlers["dungeons:totemRegeneration"] = [](endstone::Player& p) { ArtefactsGen::handle_totemRegeneration(p); };
+        artefactHandlers["dungeons:updraftTome"] = [](endstone::Player& p) { ArtefactsGen::handle_updraftTome(p); };
+        artefactHandlers["dungeons:vexingChant"] = [](endstone::Player& p) { ArtefactsGen::handle_vexingChant(p); };
+        artefactHandlers["dungeons:windHorn"] = [](endstone::Player& p) { ArtefactsGen::handle_windHorn(p); };
+        artefactHandlers["dungeons:tomeDuplication"] = [](endstone::Player& p) { ArtefactsGen::handle_tomeDuplication(p); };
+        artefactHandlers["dungeons:loveMedallion"] = [](endstone::Player& p) { ArtefactsGen::handle_loveMedallion(p); };
+        artefactHandlers["dungeons:totemSoulProtection"] = [](endstone::Player& p) { ArtefactsGen::handle_totemSoulProtection(p); };
+        artefactHandlers["dungeons:tastyBone"] = [](endstone::Player& p) { ArtefactsGen::handle_tastyBone(p); };
+        artefactHandlers["dungeons:wonderfulWheat"] = [](endstone::Player& p) { ArtefactsGen::handle_wonderfulWheat(p); };
+    }
 
     void handleArtefactUse(endstone::Player& player, const std::string& artefactId) {
-        if (artefactId == "dungeons:blast_fungus") {
-            useBlastFungus(player);
-        } else if (artefactId == "dungeons:corrupted_beacon") {
-            useCorruptedBeacon(player);
-        } else if (artefactId == "dungeons:ghost_cloak") {
-            useGhostCloak(player);
-        } else if (artefactId == "dungeons:light_feather") {
-            useLightFeather(player);
-        } else if (artefactId == "dungeons:totem_of_regeneration") {
-            useTotemOfRegeneration(player);
+        initArtefactMap();
+        auto it = artefactHandlers.find(artefactId);
+        if (it != artefactHandlers.end()) {
+            it->second(player);
         }
-    }
-
-    void useBlastFungus(endstone::Player& player) {
-        // Spawn blast fungus entity with velocity
-    }
-
-    void useCorruptedBeacon(endstone::Player& player) {
-        // Drain soul gauge, emit beam, damage entities in line of sight
-    }
-
-    void useGhostCloak(endstone::Player& player) {
-        // Apply ghost cloak buff, speed, damage resistance
-    }
-
-    void useLightFeather(endstone::Player& player) {
-        // Apply jump boost, dash forward, stun nearby mobs
-    }
-
-    void useTotemOfRegeneration(endstone::Player& player) {
-        // Spawn totem entity that heals players in radius
     }
 }
